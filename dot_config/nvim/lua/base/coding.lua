d049d55618066506efb34549342cb389f1be7d81
Jan Van Winkel <jan.van_winkel@dxplore.eu>
2023-09-24 23:51:44 +0200

    Added base nvim config
    Added basic nvim configuration based on https://github.com/alpha2phi/neovim-pde

---
+++ dot_config/nvim/lua/base/coding.lua
@@ -0,0 +1,412 @@
+return {
+  -- {
+  --   "kylechui/nvim-surround",
+  --   event = "VeryLazy",
+  --   opts = {},
+  -- },
+  {
+    "numToStr/Comment.nvim",
+    dependencies = { "JoosepAlviste/nvim-ts-context-commentstring" },
+    keys = { { "gc", mode = { "n", "v" } }, { "gcc", mode = { "n", "v" } }, { "gbc", mode = { "n", "v" } } },
+    config = function(_, _)
+      local opts = {
+        ignore = "^$",
+        pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
+      }
+      require("Comment").setup(opts)
+    end,
+  },
+  {
+    "andymass/vim-matchup",
+    event = { "BufReadPost" },
+    config = function()
+      vim.g.matchup_matchparen_offscreen = { method = "popup" }
+    end,
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    event = "InsertEnter",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      "saadparwaiz1/cmp_luasnip",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+    },
+    opts = function()
+      local cmp = require "cmp"
+      local luasnip = require "luasnip"
+      local compare = require "cmp.config.compare"
+      local source_names = {
+        nvim_lsp = "(LSP)",
+        luasnip = "(Snippet)",
+        buffer = "(Buffer)",
+        path = "(Path)",
+      }
+      local duplicates = {
+        buffer = 1,
+        path = 1,
+        nvim_lsp = 0,
+        luasnip = 1,
+      }
+      local has_words_before = function()
+        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+        return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match "%s" == nil
+      end
+
+      return {
+        completion = {
+          completeopt = "menu,menuone,noinsert",
+        },
+        sorting = {
+          priority_weight = 2,
+          comparators = {
+            compare.score,
+            compare.recently_used,
+            compare.offset,
+            compare.exact,
+            compare.kind,
+            compare.sort_text,
+            compare.length,
+            compare.order,
+          },
+        },
+        snippet = {
+          expand = function(args)
+            require("luasnip").lsp_expand(args.body)
+          end,
+        },
+        mapping = cmp.mapping.preset.insert {
+          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+          ["<C-f>"] = cmp.mapping.scroll_docs(4),
+          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<C-e>"] = cmp.mapping.abort(),
+          ["<CR>"] = cmp.mapping {
+            i = cmp.mapping.confirm { behavior = cmp.ConfirmBehavior.Replace, select = false },
+            c = function(fallback)
+              if cmp.visible() then
+                cmp.confirm { behavior = cmp.ConfirmBehavior.Replace, select = false }
+              else
+                fallback()
+              end
+            end,
+          },
+          ["<C-j>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+            elseif luasnip.expand_or_jumpable() then
+              luasnip.expand_or_jump()
+            elseif has_words_before() then
+              cmp.complete()
+            else
+              fallback()
+            end
+          end, {
+            "i",
+            "s",
+            "c",
+          }),
+          ["<C-k>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_prev_item()
+            elseif luasnip.jumpable(-1) then
+              luasnip.jump(-1)
+            else
+              fallback()
+            end
+          end, {
+            "i",
+            "s",
+            "c",
+          }),
+        },
+        sources = cmp.config.sources {
+          { name = "nvim_lsp", group_index = 1 },
+          { name = "luasnip", group_index = 1 },
+          { name = "buffer", group_index = 2 },
+          { name = "path", group_index = 2 },
+        },
+        formatting = {
+          fields = { "kind", "abbr", "menu" },
+          format = function(entry, item)
+            local duplicates_default = 0
+            item.menu = source_names[entry.source.name]
+            item.dup = duplicates[entry.source.name] or duplicates_default
+            return item
+          end,
+        },
+        experimental = {
+          hl_group = "LspCodeLens",
+          ghost_text = {},
+        },
+        window = {
+          documentation = {
+            border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
+            winhighlight = "NormalFloat:NormalFloat,FloatBorder:TelescopeBorder",
+          },
+        },
+      }
+    end,
+  },
+  {
+    "L3MON4D3/LuaSnip",
+    dependencies = {
+      {
+        "rafamadriz/friendly-snippets",
+        config = function()
+          require("luasnip.loaders.from_vscode").lazy_load()
+        end,
+      },
+    },
+    build = "make install_jsregexp",
+    opts = {
+      history = true,
+      delete_check_events = "TextChanged",
+    },
+    -- stylua: ignore
+    keys = {
+      {
+        "<C-j>",
+        function()
+          return require("luasnip").jumpable(1) and "<Plug>luasnip-jump-next" or "<C-j>"
+        end,
+        expr = true, remap = true, silent = true, mode = "i",
+      },
+      { "<C-j>", function() require("luasnip").jump(1) end, mode = "s" },
+      { "<C-k>", function() require("luasnip").jump(-1) end, mode = { "i", "s" } },
+    },
+    config = function(_, opts)
+      require("luasnip").setup(opts)
+    end,
+  },
+  {
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    opts = {
+      defaults = {
+        ["<leader>t"] = { name = "+Test" },
+        ["<leader>tt"] = { name = "+Task" },
+      },
+    },
+  },
+  {
+    "vim-test/vim-test",
+    opts = {
+      setup = {},
+    },
+    config = function(plugin, opts)
+      vim.g["test#strategy"] = "neovim"
+      vim.g["test#neovim#term_position"] = "belowright"
+      vim.g["test#neovim#preserve_screen"] = 1
+
+      -- Set up vim-test
+      for k, _ in pairs(opts.setup) do
+        opts.setup[k](plugin, opts)
+      end
+    end,
+  },
+  {
+    "nvim-neotest/neotest",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-treesitter/nvim-treesitter",
+      "antoinemadec/FixCursorHold.nvim",
+      "nvim-neotest/neotest-vim-test",
+      "vim-test/vim-test",
+      "stevearc/overseer.nvim",
+    },
+    keys = {
+      { "<leader>td", "<cmd>w|lua require('neotest').run.run({vim.fn.expand('%'), strategy = 'dap'})<cr>", desc = "Debug File" },
+      { "<leader>tL", "<cmd>w|lua require('neotest').run.run_last({strategy = 'dap'})<cr>", desc = "Debug Last" },
+      { "<leader>ta", "<cmd>w|lua require('neotest').run.attach()<cr>", desc = "Attach" },
+      { "<leader>tf", "<cmd>w|lua require('neotest').run.run(vim.fn.expand('%'))<cr>", desc = "File" },
+      { "<leader>tF", "<cmd>w|lua require('neotest').run.run(vim.loop.cwd())<cr>", desc = "All Files" },
+      { "<leader>tl", "<cmd>w|lua require('neotest').run.run_last()<cr>", desc = "Last" },
+      { "<leader>tn", "<cmd>w|lua require('neotest').run.run()<cr>", desc = "Nearest" },
+      { "<leader>tN", "<cmd>w|lua require('neotest').run.run({strategy = 'dap'})<cr>", desc = "Debug Nearest" },
+      { "<leader>to", "<cmd>w|lua require('neotest').output.open({ enter = true })<cr>", desc = "Output" },
+      { "<leader>ts", "<cmd>w|lua require('neotest').run.stop()<cr>", desc = "Stop" },
+      { "<leader>tS", "<cmd>w|lua require('neotest').summary.toggle()<cr>", desc = "Summary" },
+    },
+    opts = function()
+      return {
+        adapters = {
+          require "neotest-vim-test" {
+            ignore_file_types = { "python", "vim", "lua" },
+          },
+        },
+        status = { virtual_text = true },
+        output = { open_on_run = true },
+        -- overseer.nvim
+        consumers = {
+          overseer = require "neotest.consumers.overseer",
+        },
+        overseer = {
+          enabled = true,
+          force_default = true,
+        },
+      }
+    end,
+    config = function(_, opts)
+      local neotest_ns = vim.api.nvim_create_namespace "neotest"
+      vim.diagnostic.config({
+        virtual_text = {
+          format = function(diagnostic)
+            local message = diagnostic.message:gsub("\n", " "):gsub("\t", " "):gsub("%s+", " "):gsub("^%s+", "")
+            return message
+          end,
+        },
+      }, neotest_ns)
+      require("neotest").setup(opts)
+    end,
+  },
+  {
+    "stevearc/overseer.nvim",
+    keys = {
+      { "<leader>ttR", "<cmd>OverseerRunCmd<cr>", desc = "Run Command" },
+      { "<leader>tta", "<cmd>OverseerTaskAction<cr>", desc = "Task Action" },
+      { "<leader>ttb", "<cmd>OverseerBuild<cr>", desc = "Build" },
+      { "<leader>ttc", "<cmd>OverseerClose<cr>", desc = "Close" },
+      { "<leader>ttd", "<cmd>OverseerDeleteBundle<cr>", desc = "Delete Bundle" },
+      { "<leader>ttl", "<cmd>OverseerLoadBundle<cr>", desc = "Load Bundle" },
+      { "<leader>tto", "<cmd>OverseerOpen<cr>", desc = "Open" },
+      { "<leader>ttq", "<cmd>OverseerQuickAction<cr>", desc = "Quick Action" },
+      { "<leader>ttr", "<cmd>OverseerRun<cr>", desc = "Run" },
+      { "<leader>tts", "<cmd>OverseerSaveBundle<cr>", desc = "Save Bundle" },
+      { "<leader>ttt", "<cmd>OverseerToggle<cr>", desc = "Toggle" },
+    },
+    opts = {},
+  },
+  {
+    "anuvyklack/hydra.nvim",
+    opts = {
+      specs = {
+        test = function()
+          local cmd = require("hydra.keymap-util").cmd
+          local hint = [[
+^
+_f_: File
+_F_: All Files
+_l_: Last
+_n_: Nearest
+^
+_d_: Debug File
+_L_: Debug Last
+_N_: Debug Nearest
+^
+_o_: Output
+_S_: Summary
+^
+_a_: Attach
+_s_: Stop
+^
+^ ^  _q_: Quit 
+          ]]
+          return {
+            name = "Test",
+            hint = hint,
+            config = {
+              color = "pink",
+              invoke_on_body = true,
+              hint = {
+                border = "rounded",
+                position = "top-left",
+              },
+            },
+            mode = "n",
+            body = "<A-t>",
+            heads = {
+              { "F", cmd "w|lua require('neotest').run.run(vim.loop.cwd())", desc = "All Files" },
+              { "L", cmd "w|lua require('neotest').run.run_last({strategy = 'dap'})", desc = "Debug Last" },
+              { "N", cmd "w|lua require('neotest').run.run({strategy = 'dap'})", desc = "Debug Nearest" },
+              { "S", cmd "w|lua require('neotest').summary.toggle()", desc = "Summary" },
+              { "a", cmd "w|lua require('neotest').run.attach()", desc = "Attach" },
+              { "d", cmd "w|lua require('neotest').run.run({vim.fn.expand('%'), strategy = 'dap'}", desc = "Debug File" },
+              { "f", cmd "w|lua require('neotest').run.run(vim.fn.expand('%'))", desc = "File" },
+              { "l", cmd "w|lua require('neotest').run.run_last()", desc = "Last" },
+              { "n", cmd "w|lua require('neotest').run.run()", desc = "Nearest" },
+              { "o", cmd "w|lua require('neotest').output.open({ enter = true })", desc = "Output" },
+              { "s", cmd "w|lua require('neotest').run.stop()", desc = "Stop" },
+              { "q", nil, { exit = true, nowait = true, desc = "Exit" } },
+            },
+          }
+        end,
+      },
+    },
+  },
+  {
+    "echasnovski/mini.hipatterns",
+    event = "BufReadPre",
+    opts = function()
+      local hi = require "mini.hipatterns"
+      return {
+        highlighters = {
+          hex_color = hi.gen_highlighter.hex_color { priority = 2000 },
+        },
+      }
+    end,
+  },
+  {
+    "echasnovski/mini.ai",
+    event = "VeryLazy",
+    dependencies = { "nvim-treesitter/nvim-treesitter-textobjects" },
+    opts = function()
+      local ai = require "mini.ai"
+      return {
+        n_lines = 500,
+        custom_textobjects = {
+          o = ai.gen_spec.treesitter({
+            a = { "@block.outer", "@conditional.outer", "@loop.outer" },
+            i = { "@block.inner", "@conditional.inner", "@loop.inner" },
+          }, {}),
+          f = ai.gen_spec.treesitter({ a = "@function.outer", i = "@function.inner" }, {}),
+          c = ai.gen_spec.treesitter({ a = "@class.outer", i = "@class.inner" }, {}),
+        },
+      }
+    end,
+    config = function(_, opts)
+      require("mini.ai").setup(opts)
+      if require("utils").has "which-key.nvim" then
+        ---@type table<string, string|table>
+        local i = {
+          [" "] = "Whitespace",
+          ['"'] = 'Balanced "',
+          ["'"] = "Balanced '",
+          ["`"] = "Balanced `",
+          ["("] = "Balanced (",
+          [")"] = "Balanced ) including white-space",
+          [">"] = "Balanced > including white-space",
+          ["<lt>"] = "Balanced <",
+          ["]"] = "Balanced ] including white-space",
+          ["["] = "Balanced [",
+          ["}"] = "Balanced } including white-space",
+          ["{"] = "Balanced {",
+          ["?"] = "User Prompt",
+          _ = "Underscore",
+          a = "Argument",
+          b = "Balanced ), ], }",
+          c = "Class",
+          f = "Function",
+          o = "Block, conditional, loop",
+          q = "Quote `, \", '",
+          t = "Tag",
+        }
+        local a = vim.deepcopy(i)
+        for k, v in pairs(a) do
+          a[k] = v:gsub(" including.*", "")
+        end
+
+        local ic = vim.deepcopy(i)
+        local ac = vim.deepcopy(a)
+        for key, name in pairs { n = "Next", l = "Last" } do
+          i[key] = vim.tbl_extend("force", { name = "Inside " .. name .. " textobject" }, ic)
+          a[key] = vim.tbl_extend("force", { name = "Around " .. name .. " textobject" }, ac)
+        end
+        require("which-key").register {
+          mode = { "o", "x" },
+          i = i,
+          a = a,
+        }
+      end
+    end,
+  },
+}
---
+++ dot_config/nvim/lua/config/init.lua
@@ -0,0 +1,12 @@
+return {
+  pde = {
+    ai = true,
+    cpp = true,
+    docker = true,
+    go = false,
+    json = true,
+    lua = true,
+    python = true,
+    rust = true,
+  },
+}
