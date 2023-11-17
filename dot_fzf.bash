3fc9f280fa108027184328c4a341a5b410cdb448
Jan Van Winkel <jan@dev.lxd>
2023-11-17 20:52:34 +0000

    Corrected .fzf.bash

--- dot_fzf.bash
+++ dot_fzf.bash
@@ -1,13 +1,13 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */home/javanwin/.asdf/installs/fzf/0.43.0/bin* ]]; then
-  PATH="${PATH:+${PATH}:}/home/javanwin/.asdf/installs/fzf/0.43.0/bin"
+if [[ ! "$PATH" == *${HOME}/.asdf/installs/fzf/0.43.0/bin* ]]; then
+	PATH="${PATH:+${PATH}:}/${HOME}/.asdf/installs/fzf/0.43.0/bin"
 fi
 
 # Auto-completion
 # ---------------
-source "/home/javanwin/.asdf/installs/fzf/0.43.0/shell/completion.bash"
+source "${HOME}/.asdf/installs/fzf/0.43.0/shell/completion.bash"
 
 # Key bindings
 # ------------
-source "/home/javanwin/.asdf/installs/fzf/0.43.0/shell/key-bindings.bash"
+source "${HOME}/.asdf/installs/fzf/0.43.0/shell/key-bindings.bash"
