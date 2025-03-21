7b314107bbdda387283545bf8cb99a7e5800addf
Jan Van Winkel <jan.van_winkel@dxplore.eu>
2025-03-21 20:11:19 +0000

    Get FZF version from .tool-versions file

--- dot_fzf.bash
+++ dot_fzf.bash
@@ -1,13 +1,14 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == *${HOME}/.asdf/installs/fzf/0.43.0/bin* ]]; then
-	PATH="${PATH:+${PATH}:}/${HOME}/.asdf/installs/fzf/0.43.0/bin"
+FZF_VERSION=$(awk -v pat="fzf" '$0 ~ pat {print $2}' "$HOME/.tool-versions")
+if [[ ! "$PATH" == *${HOME}/.asdf/installs/fzf/${FZF_VERSION}/bin* ]]; then
+	PATH="${PATH:+${PATH}:}/${HOME}/.asdf/installs/fzf/${FZF_VERSION}/bin"
 fi
 
 # Auto-completion
 # ---------------
-source "${HOME}/.asdf/installs/fzf/0.43.0/shell/completion.bash"
+source "${HOME}/.asdf/installs/fzf/${FZF_VERSION}/shell/completion.bash"
 
 # Key bindings
 # ------------
-source "${HOME}/.asdf/installs/fzf/0.43.0/shell/key-bindings.bash"
+source "${HOME}/.asdf/installs/fzf/${FZF_VERSION}/shell/key-bindings.bash"
